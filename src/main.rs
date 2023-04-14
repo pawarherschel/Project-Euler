d585278aa295dedb8a1565cf14f2314800b52628
Herschel Pawar <pawarherschel@gmail.com>
2023-04-14 23:22:06 +0530

    fuck this shit

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,17 @@
-#[macro_use]
-extern crate syn;
-
 use project_euler::*;
 
-
-
-//macro_rules! answers {
-//    ($( $problem:ident ($args:expr); )*) => {{
-//        let mut answers = vec![];
-//        let mut _count = 1;
-//        $(
-//            let result = $problem($args);
-//            answers.push(format!("Problem {}: {:?}", _count, result));
-//            _count += 1;
-//        )*
-//        println!("{:?}", answers.iter().rev().collect::<Vec<_>>());
-//    }}
-//}
 macro_rules! answers {
     ($( $problem:ident ($args:expr); )*) => {{
         let mut answers = vec![];
         let mut _count = 1;
         $(
-            let result = { 
-                let problem_ident = format!("problem_{}", _count);
-                let problem_fn = ident!(problem_ident);
-                problem_fn($args)
-            };
+            let result = $problem($args);
             answers.push(format!("Problem {}: {:?}", _count, result));
             _count += 1;
-            )*
+        )*
         println!("{:?}", answers.iter().rev().collect::<Vec<_>>());
     }}
 }
-
-macro_rules! ident {
-    ($ident:expr) => { 
-        { 
-            let id = $ident;
-            syn::parse_str::<syn::Ident>(id).unwrap()
-        } 
-    };
-}
-
-
-
 
 fn main() {
     answers![
@@ -53,11 +20,4 @@ fn main() {
         problem_3(600851475143);
         problem_4(3);
     ];
-    
-//    answers![
-//        1000;
-//        4_000_000;
-//        600851475143;
-//        3
-//    ]
 }
