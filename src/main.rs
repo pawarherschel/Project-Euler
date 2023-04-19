a0ff905fb32788395eff17d7e716bae7ba8539b3
Herschel Pawar <pawarherschel@gmail.com>
2023-04-19 22:03:13 +0530

    using hashmap to store answers

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use project_euler::*;
 
+//macro_rules! answers {
+//    ($( $problem:ident ($args:expr); )*) => {{
+//        let mut answers = vec![];
+//        let mut _count = 1;
+//        $(
+//            let result = $problem($args);
+//            answers.push(format!("Problem {}: {:?}", _count, result));
+//            _count += 1;
+//        )*
+//        let answers_rev = answers.clone().into_iter().rev().collect::<Vec<_>>();
+//        dbg!(&answers_rev);
+//        answers_rev
+//    }}
+//}
+
 macro_rules! answers {
     ($( $problem:ident ($args:expr); )*) => {{
-        let mut answers = vec![];
+        let mut answers: std::collections::HashMap<i32, String> = std::collections::HashMap::new();
         let mut _count = 1;
         $(
             let result = $problem($args);
-            answers.push(format!("Problem {}: {:?}", _count, result));
+            answers.insert(_count, result.to_string());
             _count += 1;
-        )*
-        let answers_rev = answers.clone().into_iter().rev().collect::<Vec<_>>();
-        dbg!(&answers_rev);
-        answers_rev
+            )*
+        dbg!(&answers);
+        answers
     }}
 }
 
