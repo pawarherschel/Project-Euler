40f92f3a18b3b80966fa96a585090b3ccf68ec15
Herschel Pawar <pawarherschel@gmail.com>
2023-04-20 02:31:07 +0530

    smfh

--- src/zaphkiel.rs
+++ src/zaphkiel.rs
@@ -1,9 +1,6 @@
 use reqwest::blocking::Client;
 
-
-fn generate_docstring(
-    problem_number: u32,
-) -> Result<String, Box<dyn std::error::Error>> {
+fn generate_docstring(problem_number: u32) -> Result<String, Box<dyn std::error::Error>> {
     let url = format!("https://projecteuler.net/problem={}", problem_number);
     let minimal_url = format!("https://projecteuler.net/minimal={}", problem_number);
 
@@ -23,29 +20,25 @@ fn generate_docstring(
     // Fetch information from minimal version
     let minimal_html = Client::new().get(&minimal_url).send()?.text()?;
     let minimal_info: String = minimal_html
-                                .lines()
-                                .map(|line| format!("/// {}", line))
-                                .collect::<Vec<String>>()
-                                .join("\n");
-    
-                        
+        .lines()
+        .map(|line| format!("/// {}", line))
+        .collect::<Vec<String>>()
+        .join("\n");
 
     let docstring = format!("/// Problem {problem_number}: [{problem_name}] \n/// \n/// [{problem_name}]: {url} \n/// \n{minimal_info} \n/// \n/// Answer: todo!()\n");
-    
+
     Ok(docstring)
 }
 
 #[cfg(test)]
 mod tests {
-    use clipboard_win::{set_clipboard, formats};
+    use clipboard_win::{formats, set_clipboard};
 
     use super::*;
-    
+
     #[test]
     fn generate_doc() {
-        let p_no =
-        10
-        ;
+        let p_no = 10;
         let ds = generate_docstring(p_no).unwrap();
         set_clipboard(formats::Unicode, ds.clone()).expect("To set clipboard");
         print!("{ds}\n");
